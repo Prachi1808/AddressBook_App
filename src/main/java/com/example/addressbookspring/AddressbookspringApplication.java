76dff169fbdf92a066cf5884a3bd8cb2d07efc92
prachi mohanty <prachimohanty.1808@gmail.com>
2022-05-25 11:10:36 +0530

    Added curloperations and dto

--- src/main/java/com/example/addressbookspring/AddressbookspringApplication.java
+++ src/main/java/com/example/addressbookspring/AddressbookspringApplication.java
@@ -1,12 +1,11 @@
 package com.example.addressbookspring;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AddressbookspringApplication {
-
     public static void main(String[] args) {
+        System.out.println("Welcome to Addressbook App");
         SpringApplication.run(AddressbookspringApplication.class, args);
     }
 
